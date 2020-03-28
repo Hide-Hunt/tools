45dae62b18320ca8d1448988bd02232e90474add
Sylvain Willy <willy.sylvain@gmail.com>
2020-03-28 16:39:59 +0100

    Add protobuf compiler installation with pacman, apt and homebrew

--- setup.sh
+++ setup.sh
@@ -1,5 +1,18 @@
 #!/usr/bin/env bash
 python3 -m venv venv
 source venv/bin/activate
-pip install paho-mqtt protobuf
+pip install paho-mqtt
+
+if [[ -n $(command -v pacman) ]]; then
+  sudo pacman -S protobuf
+elif [[ -n $(command -v apt) ]]; then
+    sudo apt install protobuf-compiler
+elif [[ -n $(command -v brew) ]]; then
+    brew install protobuf
+else
+    echo "Missing implementation for your system, sorry! Please tell Sylvain about it"
+    exit 1
+fi
+
+pip install protobuf # Does this need to be installed after protobuf itself ? I don't know...
 ./compile_protobuf.sh
